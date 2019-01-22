373f8776839438c660b022f21ecec4209948fbe4
Christopher Adams <chrismichaeladams@gmail.com>
2019-01-21 20:40:13 -0500

    Class based and function components, time display example

---
+++ README.md
@@ -0,0 +1,11 @@
+## Install live-server
+```
+npm install -g live-server
+```
+
+## Run live server:
+```
+live-server
+```
+
+
--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 const rootElement = document.getElementById('app');
 
-const myElement = document.createElement('h1');
-myElement.className = "orange";
-myElement.innerText = 'Hello From Pluralsight';
 
-rootElement.appendChild(myElement);
+/*
+ * React Functional component 
+ */
+// const Hello = function(props) {
+//   return React.createElement(
+//       "h1", 
+//       {
+//         className: "orange"
+//       }, 
+//       "Hello world! " + props.time
+//     );
+// };
+
+/*
+ * Class based component
+ */
+class Hello extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString()
+    }
+  }
+
+  tick() {
+    this.setState(() => {
+      return({
+        time: new Date().toLocaleString()
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.internval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return React.createElement(
+        "h1", 
+        {
+          className: "orange"
+        }, 
+        "Date/Time: " + this.state.time
+    );
+  }
+};
+
+ReactDOM.render(
+  React.createElement(Hello, {time: new Date()}, null),
+  document.getElementById('app')
+);
